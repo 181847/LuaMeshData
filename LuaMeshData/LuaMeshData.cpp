7ec0aad8d8433174ef16aae9162fac339cbd1579
181847 <753090084@qq.com>
2017-11-17 20:01:02 +0800

    先尝试用C++编写面向对象的Lua模块

--- LuaMeshData/LuaMeshData.cpp
+++ LuaMeshData/LuaMeshData.cpp
@@ -1,7 +1,42 @@
 #include "LuaMeshData.h"
 
-
-int DLL_API luaopen_array(lua_State * L)
+#ifdef __cplusplus
+extern "C"
 {
+#endif
+int DLL_API luaopen_LuaMeshData(lua_State * L)
+{
+	luaL_newmetatable(L, "LoadAssets.MeshData");
+	lua_pushvalue(L, 1);
+	lua_setfield(L, -2, "__index");
+	luaL_setfuncs(L, MeshDataLib_m, 0);
+	luaL_newlib(L, MeshDataLib);
+	return 1;
+}
+#ifdef __cplusplus
+}
+#endif
 
+int lua_newMeshData(lua_State * L)
+{
+	// convert the user data,
+	// using a lambda of the struct to automatically free the object.
+	auto *pContainer = reinterpret_cast<LuaPointerContainer<GeometryGenerator::MeshData>*>
+		(lua_newuserdata(L, sizeof(LuaPointerContainer<GeometryGenerator::MeshData>)));
+
+	auto * pMeshData = new GeometryGenerator::MeshData();
+	pContainer->setPointer(pMeshData);
+
+	luaL_getmetatable(L, "LoadAssets.MeshData");
+	lua_setmetatable(L, -2);
+	
+	return 1;
+}
+
+int lua_showMeshData(lua_State * L)
+{
+	// check and get mesh data
+	auto *pMeshData = checkMeshData(L);
+	fprintf(stdout, "This is a MeshData.");
+	return 0;
 }
--- LuaMeshData/LuaMeshData.h
+++ LuaMeshData/LuaMeshData.h
@@ -1,9 +1,77 @@
 #pragma once
 #include "DLLTools.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 #include "lib\lua\lua.h"
+#include "lib\lua\lauxlib.h"
+#include "lib\lua\lualib.h"
+
+#ifdef __cplusplus
+}
+#endif
 #include "lib\DirectX12\Common\GeometryGenerator.h"
 
-int DLL_API luaopen_MeshData(lua_State * L);
+#include "lib\MyTools\Cleaner.h"
+#pragma comment(lib, "lua.lib")
+
+#define checkMeshData(L) \
+	reinterpret_cast<LuaPointerContainer<GeometryGenerator::MeshData>*>\
+	(luaL_checkudata(L, 1, "LoadAssets.MeshData"))
+
+template<typename T>
+struct LuaPointerContainer
+{
+	T * pointer;
+	// using the cleaner to free the memeroy in the pointer.
+	std::function<void(void)> cleaner;
+	LuaPointerContainer()
+	{
+		cleaner = [&this]() {delete this->pointer; };
+	}
+	~LuaPointerContainer()
+	{
+		cleaner(pointer);
+	}
+
+	// reload '->'
+	T * operator ->()
+	{
+		return pointer;
+	}
+	
+	void setPointer(T * p)
+	{
+		pointer = p;
+	}
+};
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+	int DLL_API luaopen_MeshData(lua_State * L);
+#ifdef __cplusplus
+}
+#endif
+
 
 // create a new MeshData.
-int lua_newMeshData(lua_State * L);
+static int lua_newMeshData(lua_State * L);
+
+// just show the data for test
+static int lua_showMeshData(lua_State * L);
+
+static const struct luaL_Reg MeshDataLib[] =
+{
+	{"new", lua_newMeshData},
+	{NULL, NULL}
+};
+
+static const struct luaL_Reg MeshDataLib_m[] =
+{
+	{"show", lua_showMeshData},
+	{NULL, NULL}
+};
